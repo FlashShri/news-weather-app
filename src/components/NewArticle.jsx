8e9bf8fe1ebba98082ff5b539bab0f1fdc537801
FlashShri <shrikantdhengle3101@gmail.com>
2023-12-05 12:11:47 +0530

    adding article component

--- src/components/NewArticle.jsx
+++ src/components/NewArticle.jsx
@@ -1,8 +1,24 @@
 
-// export function NewArticle(){
+import { Button, Card, Col } from "react-bootstrap";
 
-//     return (
+export function NewArticle( props ){
 
-//     );
+    return (
 
-// }
+         <Col lg={4}>
+            <Card >
+                <Card.Img variant="top" src={props.myprop.urlToImage} />
+                    <Card.Body>
+                    <Card.Title>{props.myprop.title}    </Card.Title>
+                    <Card.Text>
+          {props.myprop.description}
+                    </Card.Text>
+                    <Button variant="primary">Visit</Button>
+      </Card.Body>
+    </Card>
+                                
+            </Col>
+
+    );
+
+}
--- src/components/TopHeadline.jsx
+++ src/components/TopHeadline.jsx
@@ -2,7 +2,8 @@
 import { useEffect, useState } from "react";
 import { Header } from "./Header";
 import { fetchTopHeadlines } from "../services/NewsService";
-import { Col, Container, Row, Card,  Button} from "react-bootstrap";
+import {  Container, Row} from "react-bootstrap";
+import { NewArticle } from "./NewArticle";
 
 
 export function TopHeadline (){
@@ -42,19 +43,7 @@ export function TopHeadline (){
                 {
                     articles.map((article)=>{
                         return(
-                            <Col lg={4}>
-                                    <Card >
-      <Card.Img variant="top" src={article.urlToImage} />
-      <Card.Body>
-        <Card.Title>{article.title}</Card.Title>
-        <Card.Text>
-          {article.description}
-        </Card.Text>
-        <Button variant="primary">Visit</Button>
-      </Card.Body>
-    </Card>
-                                
-                            </Col>
+                            <NewArticle myprop={article} my2ndprop ={article}></NewArticle> 
                         )
                     })
                 }
